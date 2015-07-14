356484ba49dc678477be54a1233df7eb9f7806ce
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2015-07-14 10:26:30 +0200

    add colorPickerComponent

---
+++ app/scripts/directives/colorPickerComponent.js
@@ -0,0 +1,25 @@
+(function(module) {
+
+    module.directive('colorPickerComponent', function() {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/directives/colorPickerComponent.html',
+            scope: {},
+            controller: ColorPickerComponentCtrl,
+            controllerAs: 'ColorPickerComponentCtrl'
+        };
+
+    });
+
+    function ColorPickerComponentCtrl() {
+        this.r = 0;
+        this.g = 0;
+        this.b = 255;
+    }
+
+    ColorPickerComponentCtrl.prototype.sayHello = function() {
+        console.log('Hello', this.r, this.g, this.b);
+    };
+
+})(angular.module('ciApp'));
